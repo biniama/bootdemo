a483fac3c9afac56a0f260e8185e0d75cf916c8d
Biniam Asnake Kefale <biniamasnake@gmail.com>
2016-02-10 00:25:46 +0100

    HelloController added and running

---
+++ src/main/groovy/com/hasset/BootdemoApplication.groovy
@@ -0,0 +1,22 @@
+package com.hasset
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ApplicationContext
+
+@SpringBootApplication
+class BootdemoApplication {
+
+	static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(BootdemoApplication, args)
+
+        println("Let's inspect the beans provided by Spring Boot:")
+
+        String[] beanNames = ctx.getBeanDefinitionNames()
+
+        beanNames.sort().each {
+            println it
+        }
+	}
+}
---
+++ src/main/groovy/com/hasset/controllers/HelloController.groovy
@@ -0,0 +1,16 @@
+package com.hasset.controllers
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by biniamasnake on 09/02/16.
+ */
+@RestController
+class HelloController {
+
+    @RequestMapping("/")
+    def index() {
+        return [ greeting: "Greetings from Spring Boot!" ]
+    }
+}
